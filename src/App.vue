e3e2a8ed141806858fdd6280d01477561b22c42d
Dieter <dburgstaller@gmx.de>
2023-01-29 09:06:57 +0100

    Initial commit Vue Masterclass Section 2 Intro to Vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+    <HelloWorld msg="Nice job"> </HelloWorld>
+  </div>
+</template>
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped></style>
